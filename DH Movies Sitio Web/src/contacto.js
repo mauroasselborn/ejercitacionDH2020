c282c5281a334c08a9f9846a115a6def85ef9be5
MaUrO <mauasselborn@gmail.com>
2020-04-07 15:30:44 -0300

    Se Completan los requerimientos de la seccion correspondiente

---
+++ DH Movies Sitio Web/src/contacto.js
@@ -0,0 +1,28 @@
+let contenidoWeb = `
+<head>
+    <meta charset="utf-8">
+</head>
+<h1>Contáctanos</h1>
+<hr>
+<p>
+    ¿Tenés algo para contarnos? Nos encanta escuchar a nuestros
+    clientes. Si deseas contactarnos podés escribirnos al siguiente email:
+    dhmovies@digitalhouse.com o en las redes sociales. Envianos tu consulta,
+    sugerencia o reclamo y será respondido a la brevedad posible. Recordá que
+    también podes consultar la sección de Preguntas Frecuentes para obtener
+    respuestas inmediatas a los problemas más comunes.
+</p>
+<hr>
+<p>
+    Recordá que podés visitar las secciones:
+   <ul>
+        <li style="display:inline-block; margin-right: 50px;list-style:none"><a href="/">Home</a></li>
+        <li style="display:inline-block; margin-right: 50px;list-style:none"><a href="en-cartelera">En Cartelera</a></li>
+        <li style="display:inline-block; margin-right: 50px;list-style:none"><a href="mas-votadas">Más Votadas</a></li>
+        <li style="display:inline-block; margin-right: 50px;list-style:none"><a href="sucursales">Sucursales</a></li>
+        <li style="display:inline-block; margin-right: 50px;list-style:none"><a href="contacto">Contacto</a></li>
+        <li style="display:inline-block; margin-right: 50px;list-style:none"><a href="preguntas-frecuentes">Preguntas Frecuentes</a></li>
+    </ul>
+</p>`;
+
+module.exports = contenidoWeb;
