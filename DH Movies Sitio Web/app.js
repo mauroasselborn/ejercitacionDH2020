c9d55f531b5ee405355cce77ed9cc3439f8d7e68
MaUrO <mauasselborn@gmail.com>
2020-04-07 14:03:11 -0300

    App levanta el Server

--- DH Movies Sitio Web/app.js
+++ DH Movies Sitio Web/app.js
@@ -1,18 +1,11 @@
-/* const http = require("http");
+const http = require("http");
 const router = require("./router");
 
-http.createServer((req, res) => {
-    res.writeHead(200, { contentType: "text/plain" });
-    router.rutas(req, res);
-}).listen(3030, "localhost", () => {
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "contentType": "text/plain" });
+    res.end(router.rutas(req));
+});
+
+server.listen(3030, "localhost", () => {
     console.log("servercorriendo correctamente en el puerto 3030");
 });
-*/
-const fs = require("fs");
-
-let faqs = JSON.parse(fs.readFileSync(__dirname + "/data/faqs.json"), "utf-8");
-
-for (const key in faqs.faqs) {
-    console.log(faqs.faqs[key].faq_title);
-    console.log(faqs.faqs[key].faq_answer);
-}
