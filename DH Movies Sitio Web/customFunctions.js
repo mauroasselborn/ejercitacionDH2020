cea003a0014c760bd50e22daddf3da2f86b74460
MaUrO <mauasselborn@gmail.com>
2020-04-07 13:55:37 -0300

    Se Actualiza y optimiza la forma de exportar las funciones

--- DH Movies Sitio Web/customFunctions.js
+++ DH Movies Sitio Web/customFunctions.js
@@ -2,12 +2,8 @@ const fs = require("fs");
 
 const leerArchivo = (ruta) => JSON.parse(fs.readFileSync(ruta, "utf-8"));
 
-const getMovies = leerArchivo(__dirname + "/data/movies.json");
-const getFaqs = leerArchivo(__dirname + "/data/faqs.json");
-const getTheaters = leerArchivo(__dirname + "/data/theaters.json");
-
 module.exports = {
-    getMovies: getMovies,
-    getFaqs: getFaqs,
-    getTheaters: getTheaters,
+    getMovies: () => leerArchivo(__dirname + "/data/movies.json"),
+    getFaqs: () => leerArchivo(__dirname + "/data/faqs.json"),
+    getTheaters: () => leerArchivo(__dirname + "/data/theaters.json"),
 };
