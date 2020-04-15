8f8376d39dfa5c7dee3da7402403da472d239aa8
MaUrO <mauasselborn@gmail.com>
2020-04-14 22:03:48 -0300

    Modificanmdo directorio

--- DH Movies Sitio Web - Version ExpressJS/app.js
+++ DH Movies Sitio Web - Version ExpressJS/app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const path = require("path")
 
 const app = express()
 const router = express.Router()
@@ -6,5 +7,8 @@ const router = express.Router()
 // view engine setup
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
+app.set("port", process.env.PORT || 3000)
+
+//app.use()
 
 module.exports = app
